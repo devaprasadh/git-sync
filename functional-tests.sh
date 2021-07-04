4b7277fafd767c71474293e1a24fca950b60a18a
devatherock <devatherock@gmail.com>
2021-07-04 17:45:11 -0500

    Copied .git folder to test folder

--- functional-tests.sh
+++ functional-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 mkdir git-sync-test
-cp -r . git-sync-test
+cp -r * git-sync-test
+cp -r .git git-sync-test
 cd git-sync-test
 export CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 git remote set-url origin "https://git-sync-token:$GIT_TOKEN@github.com/devatherock/git-sync.git"
